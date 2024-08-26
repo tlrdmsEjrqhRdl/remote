3e71975934e29edfba941983fc085cc3cf94fd14
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-08-26 17:54:57 +0900

    4rkwl choice final

--- doorcloth/doorcloth2/doorcloth2.js
+++ doorcloth/doorcloth2/doorcloth2.js
@@ -5,3 +5,13 @@ fileInput.addEventListener('change', () => {
   var fileName = fileInput.files[0].name;
   fileNameDisplay.value = fileName;
 });
+
+function checkOnlyOne(element) {
+  const checkboxes = document.getElementsByName('gubunchoice');
+
+  checkboxes.forEach((cb) => {
+    cb.checked = false;
+  });
+
+  element.checked = true;
+}
